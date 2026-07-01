301ccda30fab510101196502923a135d93420ff1
AamerRiaz15 <aamerriaz22@gmail.com>
2026-07-01 14:13:19 +0100

    Implement dynamic array allocation and manipulation

--- intermediate/pointers/dynamic_array_allocation/main.cpp
+++ intermediate/pointers/dynamic_array_allocation/main.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
 
+int get_allocation_size();
+int read_input();
+void fill_array(int* arr, int size);
+void print_array(int* arr, int size);
+
+int main() {
+    int size = get_allocation_size();
+    
+    int* arr = new int[size]; // Allocated array on heap
+    fill_array(arr, size);
+    print_array(arr, size);
+
+    delete[] arr;
+
+    return 0;
+}
+
+int get_allocation_size() {
+    std::cout << "Enter numbers for allocation: ";
+    int user_input = read_input();
+
+    return user_input;
+}
+
+int read_input() {
+    int num {};
+    std::cin >> num;
+    
+    return num; 
+}
+
+void fill_array(int* arr, int size) {
+    for (size_t i = 0; i < size; ++i) {
+        *(arr + i) = (i + 1) * 10;
+    }
+}
+
+void print_array(int* arr, int size) {
+    std::cout << "Array contents: ";
+    for (size_t i = 0; i < size; ++i) {
+        std::cout << *(arr + i) << " "; 
+    }
+    std::cout << "\n";
+} 
