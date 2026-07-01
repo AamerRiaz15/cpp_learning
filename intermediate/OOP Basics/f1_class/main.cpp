ff606d1a41949a99595d2c319b2edc5c078272db
AamerRiaz15 <aamerriaz22@gmail.com>
2026-07-01 13:10:16 +0100

    Add F1 class with speed and model management
    Implemented a class F1 with methods to manipulate speed and model.

--- intermediate/OOP Basics/f1_class/main.cpp
+++ intermediate/OOP Basics/f1_class/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
 
+class F1 {
+private:
+    double speed;
+    std::string model;
+public:
+    void increase_speed(double speed_val) {speed += speed_val;}
+    void decrease_speed(double speed_val) {speed -= speed_val;}
+    void change_model(std::string model_val) {model = model_val;}
+    
+    F1(double speed_val = 0.0, std::string model_val = "None");
+};
+
+F1::F1(double speed_val, std::string model_val)
+    : speed{speed_val}, model{model_val} {
+        std::cout << "Two args constructor called." << "\n";
+}
+
+int main() {
+    F1 w11{10.0};
+
+    return 0;
+}
