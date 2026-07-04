89a7af0d01d4d69e181de52ad04b29757feeee78
AamerRiaz15 <aamerriaz22@gmail.com>
2026-07-04 16:12:12 +0100

    Add Player class with default constructor arguments

--- intermediate/OOP Basics/player_class_default_arguments/Player.h
+++ intermediate/OOP Basics/player_class_default_arguments/Player.h
@@ -1,1 +1,22 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include <string>
+
+class Player {
+private:
+    double health;
+    int xp;
+    std::string name;
+public:
+    double get_health();
+    int get_xp();
+    std::string get_name();
+
+    void heal(double heal_val);
+    void take_damage(double damage_val);
+
+    Player(double health_val = 0, int xp_val = 0, std::string name_val = "None");
+};
+
+#endif
