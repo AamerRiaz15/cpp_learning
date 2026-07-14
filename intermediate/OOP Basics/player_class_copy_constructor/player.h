bbd4203e9d03ea4b129d02c924d9dc73c2fee598
AamerRiaz15 <aamerriaz22@gmail.com>
2026-07-14 16:05:24 +0100

    Update player.h

--- intermediate/OOP Basics/player_class_copy_constructor/player.h
+++ intermediate/OOP Basics/player_class_copy_constructor/player.h
@@ -1,1 +1,26 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include <string>
+
+class Player {
+private:
+    int health;
+    int xp;
+    std::string name;
+public:
+    int get_health();
+    int get_xp();
+    std::string get_name();
+
+    void set_health(int health_val);
+    
+    Player();
+    Player(int health_val);
+    Player(int health_val, int xp_val);
+    Player(int health_val, int xp_val, std::string name_val);
+    Player(const Player &source);
+    ~Player();
+};
+
+#endif
