c1ab96b00c45b82c887216c46058652a07568235
AamerRiaz15 <aamerriaz22@gmail.com>
2026-08-10 16:17:37 +0100

    Refactor

--- intermediate/OOP Basics/static_variables_functions/Player.cpp
+++ intermediate/OOP Basics/static_variables_functions/Player.cpp
@@ -3,25 +3,26 @@
 
 int Player::num_players{0};
 
-int Player::get_health() {
-    return health;
-}
-
 std::string Player::get_name() {
     return name;
+}
+
+int Player::get_health() {
+    return health;
 }
 
 int Player::get_xp() {
     return xp;
 }
 
-Player::Player(int health_val, std::string name_val, int xp_val) 
-    : health{health_val}, name{name_val}, xp{xp_val} {
+Player::Player(std::string name_val, int health_val, int xp_val) 
+    :  name{name_val}, health{health_val}, xp{xp_val} {
         num_players++;
 }
 
 Player::Player(const Player &source) 
-    : health{source.health}, name{source.name}, xp{source.xp} {
+    : name{source.name}, health{source.health}, xp{source.xp} {
+        num_players++;
 }
 
 Player::~Player() {
