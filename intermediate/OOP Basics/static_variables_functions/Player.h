fa40ea62dcd3904ffe55f156f67060cfe6f90e2e
AamerRiaz15 <aamerriaz22@gmail.com>
2026-08-10 16:18:14 +0100

    Add Player class with static member and methods

--- intermediate/OOP Basics/static_variables_functions/Player.h
+++ intermediate/OOP Basics/static_variables_functions/Player.h
@@ -1,1 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include <string>
+
+class Player {
+private:
+    static int num_players;
+
+    std::string name;
+    int health;
+    int xp;
+public:
+    int get_health();
+    std::string get_name();
+    int get_xp();
+
+    Player(std::string name_val = "None", int health = 100, int xp_val = 0);
+    Player(const Player &source);
+    ~Player();
+
+    static int get_num_players();
+};
+
+#endif
