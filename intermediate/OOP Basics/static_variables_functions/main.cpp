4adcaaca451eb297273ffb7a73705d243c4c324f
AamerRiaz15 <aamerriaz22@gmail.com>
2026-08-10 16:19:00 +0100

    Implement main

--- intermediate/OOP Basics/static_variables_functions/main.cpp
+++ intermediate/OOP Basics/static_variables_functions/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Player.h"
 
+void display_active_players();
+
+int main() {
+    display_active_players();
+    Player hero{"Hero"};
+    display_active_players();
+
+    return 0;
+}
+
+void display_active_players() {
+    std::cout << Player::get_num_players() << "\n";
+}
