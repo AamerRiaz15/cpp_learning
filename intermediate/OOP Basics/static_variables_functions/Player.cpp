13191f830322a6152d7735589d6988520213f53c
AamerRiaz15 <aamerriaz22@gmail.com>
2026-08-10 16:04:02 +0100

    Implement Player class with static member functions

--- intermediate/OOP Basics/static_variables_functions/Player.cpp
+++ intermediate/OOP Basics/static_variables_functions/Player.cpp
@@ -1,1 +1,33 @@
+#include "Player.h"
+#include <string>
 
+int Player::num_players{0};
+
+int Player::get_health() {
+    return health;
+}
+
+std::string Player::get_name() {
+    return name;
+}
+
+int Player::get_xp() {
+    return xp;
+}
+
+Player::Player(int health_val, std::string name_val, int xp_val) 
+    : health{health_val}, name{name_val}, xp{xp_val} {
+        num_players++;
+}
+
+Player::Player(const Player &source) 
+    : health{source.health}, name{source.name}, xp{source.xp} {
+}
+
+Player::~Player() {
+    num_players--;
+}
+
+int Player::get_num_players() {
+    return num_players;
+}
